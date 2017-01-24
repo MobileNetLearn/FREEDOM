807bba9cefc24cee453e74af1e3b7d42b9b0c872
Jared Allard <jaredallard@outlook.com>
2017-01-24 21:54:10 +0000

    dockerfile: patch hostapd and inject configs, init handles, well, init

--- init.sh
+++ init.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env sh
 
+
+out() {
+	echo "INIT: $*"
+}
+
+onexit() {
+	out "got SIGTERM/KILL"
+	/etc/init.d/dnsmasq stop
+	/etc/init.d/hostapd stop
+	/etc/init.d/dbus stop
+}
+
 # Start haveged
 haveged -F >/var/log/haveged.log 2>/var/log/haveged.log
 
@@ -9,8 +21,12 @@ rfkill unblock wlan
 # Set IP
 # ifconfig wlan1 10.1.1.1/24
 
-# Wait?
-sleep 2
+/etc/init.d/dbus start
+/etc/init.d/dnsmasq start
+sleep 1
+/etc/init.d/hostapd start
+
+# TODO: Network configuration
 
 # Enable IP Forwarding
 echo 1 > /proc/sys/net/ipv4/ip_forward
@@ -18,10 +34,11 @@ echo 1 > /proc/sys/net/ipv4/ip_forward
 # Forwarding on eth0
 iptables -t nat -C POSTROUTING -o eth0 -j MASQUERADE
 
-# Start DBus
-/etc/init.d/dbus start
+# On Exit.
+trap onexit SIGTERM
+trap onexit SIGKILL
 
-# Nothing
+# we do nothing!
 while true
 do
 	sleep 1
