69ce9c36141cec1436f4a3b0af4ed5136031dcb4
Jared Allard <jaredallard@outlook.com>
2017-02-08 10:40:22 -0800

    init: systemd support back in

--- scripts/boot.sh
+++ scripts/boot.sh
@@ -96,7 +96,9 @@ cat /etc/resolv.conf
 build
 
 # Pull the Docker Image
-CMD="docker run -t -d -v /boot/logs:/logs -v /etc/resolv.conf:/etc/resolv.conf -v /sys/fs/cgroup:/sys/fs/cgroup -v /boot/config:/config --privileged --net=host test_priv"
+CMD="docker run -t -d -v /boot/logs:/logs -v /etc/resolv.conf:/etc/resolv.conf -v /boot/config:/config --privileged --net=host test_priv"
+
+out "${CMD}"
 CONTAINERID="$(${CMD} | tr -d '\n')"
 
 out "container ID: ${CONTAINERID}"
@@ -113,7 +115,6 @@ until ping -c1 172.10.0.1 &>/dev/null; do :; done
 
 
 ################################################################################
-#
 # POST VPN SETUP
 
 # Delete nameservers on eth0
